a5c0590761d3cea70c3f0ca2cda341c9582b6517
Leonardo C <leonardocoel98@gmail.com>
2022-04-07 16:40:15 -0300

    feat: Contador iniciado

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -2,9 +2,52 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Game extends Component {
-  render() {
-    return (
-      <Header />
-    );
+  constructor() {
+    super();
+    this.state = {
+      timer: 30,
+      isDisabled: false,
+      // disable: false,
+    };
   }
+
+  componentDidMount() {
+    const ONE_SECOND = 1000;
+    this.intervalId = setInterval(() => {
+      this.setState((prev) => ({ timer: prev.timer - 1 }));
+    }, ONE_SECOND);
+  }
+
+  componentDidUpdate(props, prevState) {
+    this.handleTimer(prevState);
+  }
+
+handleTimer = (prevState) => {
+  const ZERO = 0;
+  const { timer, isDisabled } = this.state;
+  if (prevState.timer !== timer && timer === ZERO) {
+    clearInterval(this.intervalId);
+    // this.setState(() => ({ disable: true }));
+  }
+  if (isDisabled) {
+    clearInterval(this.intervalId);
+  }
+}
+
+render() {
+  const { timer } = this.state;
+  return (
+    <>
+      <Header />
+      <p>{timer}</p>
+      <button
+        type="button"
+        onClick={ () => this.setState({ isDisabled: true }) }
+      >
+        OK
+
+      </button>
+    </>
+  );
+}
 }
