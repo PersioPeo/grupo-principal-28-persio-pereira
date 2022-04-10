c64922b2b7c7ff52ec92cf9133a9ecb2830487f1
Diogo <diogompantaleao@gmail.com>
2022-04-10 19:06:17 -0300

    feat(17): Botão para jogar novamente criado

--- src/Pages/Ranking.jsx
+++ src/Pages/Ranking.jsx
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
+import { questionIndex } from '../actions';
 
-export default class Ranking extends Component {
+class Ranking extends Component {
   render() {
-    return <div data-testid="ranking-title">Ranking</div>;
+    return (
+      <div data-testid="ranking-title">
+        <button
+          onClick={ () => {
+            const { history, dispatch } = this.props;
+            dispatch(questionIndex(0));
+            history.push('/');
+          } }
+          type="button"
+          data-testid="btn-go-home"
+        >
+          Play Again
+        </button>
+        Ranking
+      </div>
+    );
   }
 }
+
+Ranking.propTypes = {
+  history: func,
+  dispatch: func,
+}.isDisabled;
+
+const mapStateToProps = ({ player }) => ({
+  // [
+  //   { name: nome - da - pessoa, score: 10, picture: url - da - foto - no - gravatar }
+  // ]
+  player,
+});
+
+export default connect(mapStateToProps)(Ranking);
